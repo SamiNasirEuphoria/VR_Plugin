7b859255031e40f0ae2ae955e2bc2ce75d15839e
sami nasir <sami.nasir@euphoria.pk>
2024-05-22 13:12:36 +0500

    Done

---
+++ Assets/Scripts/HomeObjectController.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class HomeObjectController : MonoBehaviour
+{
+    public GameObject innerSphere;
+    private void OnEnable()
+    {
+        innerSphere.SetActive(true);
+    }
+}
